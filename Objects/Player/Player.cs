9d7ac46bd9a2177c8ca4c7242b797238c72ebb05
kodix86 <cam.peterson@protonmail.com>
2022-05-07 19:59:56 -0500

    Basic Player Controller

---
+++ Objects/Player/Player.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+    [Export(PropertyHint.Range, "0,200")]
+    private int _moveSpeed = 100;
+    [Export(PropertyHint.Range, "0,1000")]
+    private int _jumpSpeed = 550;
+    [Export(PropertyHint.Range, "1000,3000")]
+    private int _gravity = 2000;
+
+    private Vector2 _velocity = Vector2.Zero;
+
+    public override void _PhysicsProcess(float delta)
+    {
+        _velocity.x = 0; //Reset Horizontal
+
+        if (Input.IsActionPressed("moveRight"))
+        {
+            _velocity.x += _moveSpeed;
+        }
+        
+        if (Input.IsActionPressed("moveLeft"))
+        {
+            _velocity.x -= _moveSpeed;
+        }
+        
+        if (Input.IsActionPressed("jump"))
+        {
+            if (IsOnFloor())
+            {
+                _velocity.y -= _jumpSpeed;
+            }
+        }
+
+        _velocity.y += _gravity * delta;
+
+        _velocity = MoveAndSlide(_velocity, Vector2.Up);
+    }
+    
+}

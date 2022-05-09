6083b5c3c508c53a3f9678aabe4dfb925f3256d2
Kodix86 <cam.peterson@protonmail.com>
2022-05-09 11:04:58 -0500

    Very buggy and poor implementation of most movement.
    A simple wall kick / jump has been made. As well as simple momentum. I am going to re-write everything that presently exists to be more generalized and more readable.

--- Objects/Player/Player.cs
+++ Objects/Player/Player.cs
@@ -3,48 +3,62 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    [Export(PropertyHint.Range, "0,200")]
-    private int _moveSpeed = 100;
-    [Export(PropertyHint.Range, "0,1000")]
-    private int _jumpSpeed = 550;
-    [Export(PropertyHint.Range, "1000,3000")]
-    private int _gravity = 2000;
+	[Export(PropertyHint.Range, "0,200")]
+	private int _moveSpeed = 100;
+	[Export(PropertyHint.Range, "0,1000")]
+	private int _jumpSpeed = 550;
+	[Export(PropertyHint.Range, "1000,3000")]
+	private int _gravity = 2000;
 
-    private Vector2 _velocity = Vector2.Zero;
+	private Vector2 _velocity = Vector2.Zero;
 
-    public override void _PhysicsProcess(float delta)
-    {
-        _velocity.x = 0; //Reset Horizontal
+	public override void _PhysicsProcess(float delta)
+	{
+		_velocity.x /= 1.5f; //Reset Horizontal
 
-        if (Input.IsActionPressed("moveRight"))
-        {
-            if (Input.IsActionPressed("sprint"))
-            {
-                _velocity.x += _moveSpeed;
-            }
-            _velocity.x += _moveSpeed;
-        }
-        
-        if (Input.IsActionPressed("moveLeft"))
-        {
-            if (Input.IsActionPressed("sprint"))
-            {
-                _velocity.x -= _moveSpeed;
-            }
-            _velocity.x -= _moveSpeed;
-        }
-        
-        if (Input.IsActionPressed("jump"))
-        {
-            if (IsOnFloor())
-            {
-                _velocity.y -= _jumpSpeed;
-            }
-        }
+		if (Input.IsActionPressed("moveRight"))
+		{
+			if (Input.IsActionPressed("sprint"))
+			{
+				_velocity.x += _moveSpeed;
+			}
+			_velocity.x += _moveSpeed;
+		}
+		
+		if (Input.IsActionPressed("moveLeft"))
+		{
+			if (Input.IsActionPressed("sprint"))
+			{
+				_velocity.x -= _moveSpeed;
+			}
+			_velocity.x -= _moveSpeed;
+		}
+		
+		if (Input.IsActionPressed("jump"))
+		{
+			if (IsOnFloor())
+			{
+				_velocity.y -= _jumpSpeed;
+			}
+			else {
+				if (IsOnWall()) {
+					_velocity.y -= _jumpSpeed / 2f;
+					_velocity.x += _jumpSpeed * 2f;
+				}
+			}
 
-        _velocity.y += _gravity * delta;
+		}
 
-        _velocity = MoveAndSlide(_velocity, Vector2.Up);
-    }
-    
+		if (!IsOnFloor()) { //This is used so we don't apply gravity when we are on slopes.
+			_velocity.y += _gravity * delta;
+		}
+
+		if (IsOnWall()) {
+			Console.WriteLine("WALL RIDAAA");
+			_velocity.y += (_gravity / 2f) * delta;
+		}
+
+		_velocity = MoveAndSlide(_velocity, Vector2.Up);
+	}
+	
 }
